ab0047b7b8beb9a690450885afa90772d08c37f2
Brian Jensen <bjensen@princeton.edu>
2025-11-08 19:16:25 -0500

    Add initialization file and constants class

---
+++ src/test_particle_sim_1d/initialization.py
@@ -0,0 +1,17 @@
+"""
+initialization.py
+"""
+
+from __future__ import annotations
+
+
+class constants:
+    """Physical constants for use in the simulation"""
+
+    q_e = 1.602176634e-19  # C
+    m_e = 9.1093837015e-31  # kg
+    m_p = 1.67262192369e-27  # kg
+    ep0 = 8.8541878128e-12  # F/m
+    mu0 = 1.25663706212e-6  # N/A^2
+    c = 299792458.0  # m/s
+    kb = 1.380649e-23  # J/K
